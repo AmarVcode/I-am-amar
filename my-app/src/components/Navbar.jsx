850b4e1d00e1b70e0d42018203aa1b7023b9f063
amaryamunavishwakarma <98891008+amaryamunavishwakarma@users.noreply.github.com>
2022-09-24 21:33:53 +0530

    deleted

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -2,17 +2,17 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
-
 function Navigation(){
 
 return(
     <Navbar bg="dark" variant="dark">
     <Container>
-      <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+      <Navbar.Brand href="#home">A-V-Code</Navbar.Brand>
       <Nav className="me-auto">
         <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#features">Features</Nav.Link>
-        <Nav.Link href="#pricing">Pricing</Nav.Link>
+        <Nav.Link href="#about">About</Nav.Link>
+        <Nav.Link href="#projects">My Projects</Nav.Link>
+        <Nav.Link href="#contact">Contact Me</Nav.Link>
       </Nav>
     </Container>
   </Navbar>
