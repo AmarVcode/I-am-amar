bc03b6f634bd7284b271de9881e0cf6b91241ac6
@amarvcode <amarvcode@gmail.com>
2022-12-06 22:07:44 +0530

    create app

---
+++ my-app/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './mycomponents/navbar/navbar.component';
+import { FooterComponent } from './mycomponents/footer/footer.component';
+import { HomeComponent } from './mycomponents/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
