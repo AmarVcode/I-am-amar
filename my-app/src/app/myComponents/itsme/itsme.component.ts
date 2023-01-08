888b9539b48a09fe3e6289da62dfa52d5ec04cff
Amar <108946547+AmarVcode@users.noreply.github.com>
2023-01-08 17:00:04 -0500

    updated

---
+++ my-app/src/app/myComponents/itsme/itsme.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-itsme',
+  templateUrl: './itsme.component.html',
+  styleUrls: ['./itsme.component.scss']
+})
+export class ItsmeComponent {
+
+}
